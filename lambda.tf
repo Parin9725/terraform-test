87f89fbc5c4c931f2b88b533388358a85577c4af
root <root@ip-172-31-41-112.us-east-2.compute.internal>
2020-11-17 10:55:48 +0000

    lambda create

--- lambda.tf
+++ lambda.tf
@@ -19,6 +19,7 @@ EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
   function_name = "parin"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "exports.test"
