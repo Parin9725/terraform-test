788cc34823455e5502bb7480b385521daf46a6ec
root <root@ip-172-31-41-112.us-east-2.compute.internal>
2020-11-17 09:45:39 +0000

    123

--- main.tf
+++ main.tf
@@ -21,19 +21,21 @@ EOF
 resource "aws_iam_policy" "policy" {
   name        = "test-policy"
   description = "A test policy"
+
   policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Action": "ec2:DescribeInstances",
-            "Resource": "*"
-        }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
