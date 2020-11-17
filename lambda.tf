ee9f4b910171e6a3056f4f004a7ccee00bc97788
root <root@ip-172-31-41-112.us-east-2.compute.internal>
2020-11-17 12:25:42 +0000

    24325

--- lambda.tf
+++ lambda.tf
@@ -24,6 +24,7 @@ resource "aws_lambda_function" "test_lambda" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "exports.test"
 
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
   runtime = "dotnetcore3.1"
 
   environment {
