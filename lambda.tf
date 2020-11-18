616dbf5e1b125bbba397403f02c3b7c13a12f357
root <root@ip-172-31-41-112.us-east-2.compute.internal>
2020-11-18 09:32:50 +0000

    IAM policy created

--- lambda.tf
+++ lambda.tf
@@ -72,3 +72,10 @@ resource "aws_lambda_function" "test_lambda" {
   memory_size   = 128
 
 }
+
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  event_source_arn = "${aws_sqs_queue.terraform_queue.arn}"
+  enabled          = true
+  function_name    = "${aws_lambda_function.test_lambda.arn}"
+  batch_size       = 1
+}
