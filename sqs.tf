90c8a3659e9b74fe0a11524e896f3d9cf3716256
root <root@ip-172-31-41-112.us-east-2.compute.internal>
2020-11-18 09:43:04 +0000

    IAM policy created

--- sqs.tf
+++ sqs.tf
@@ -1,3 +1,4 @@
 resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "terraform-example-queue"
+  name                       = "terraform-example-queue"
+  visibility_timeout_seconds = 600
 }
