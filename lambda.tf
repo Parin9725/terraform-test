3ad1169c4177cc0769b4dac27a5b912be5c8954a
root <root@ip-172-31-41-112.us-east-2.compute.internal>
2020-11-18 08:59:03 +0000

    event source mapping

--- lambda.tf
+++ lambda.tf
@@ -73,3 +73,7 @@ resource "aws_lambda_function" "test_lambda" {
 
 }
 
+resource "aws_lambda_event_source_mapping" "example" {
+  event_source_arn = aws_sqs_queue.terraform_queue.arn
+  function_name    = aws_lambda_function.test_lambda.arn
+}
